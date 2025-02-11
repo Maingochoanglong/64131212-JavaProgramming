c849355c6b870cf595b107decd738ee4c9d972c1
MaiNgocHoangLong-edu <125291302+Maingochoanglong@users.noreply.github.com>
2025-02-12 01:13:42 +0700

    Hello World

---
+++ HelloWorld/src/MyPackage/HelloWorld.java
@@ -0,0 +1,10 @@
+package MyPackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World By Mai Ngoc Hoang Long - 64131212");
+	}
+
+}
